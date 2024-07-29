249be4d5fc611cf1c95c5386b49e12645d1e7bca
Aryan49SM <aryansm832@gmail.com>
2024-07-29 22:43:47 +0530

    :sparkles: socket.io added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,11 +8,12 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import {app, server} from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+// const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -25,7 +27,13 @@ app.use("/api/users", userRoutes);
 //     res.send("Hello World how are you");
 // });
 
-app.listen(PORT, () => {
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
