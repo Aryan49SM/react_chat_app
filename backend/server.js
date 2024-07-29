7bdcfbade07b7428465150513d59df3137f302a4
Aryan49SM <aryansm832@gmail.com>
2024-07-29 23:43:28 +0530

    :rocket: Deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import {app, server} from "./socket/socket.js";
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -22,16 +22,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req,res) => {
-//     // root route http://localhost:5000/
-//     res.send("Hello World how are you");
-// });
+// Run frontend on server
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
