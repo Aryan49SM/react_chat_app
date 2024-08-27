62b3f64bf654412d3131b127c9eb78eff25d55b6
Aryan49SM <aryansm832@gmail.com>
2024-08-27 13:20:10 +0530

    :bug: Fix: Message receiving Problem

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -3,27 +3,34 @@ import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 import useListenMessages from "../../hooks/useListenMessages";
+import useConversation from "../../zustand/useConversation";
 
 const Messages = () => {
 	const { messages, loading } = useGetMessages();
 	useListenMessages();
 	const lastMessageRef = useRef();
+	const {selectedConversation } = useConversation();
 
 	useEffect(() => {
 		setTimeout(() => {
 			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
 		}, 100);
 	}, [messages]);
-
+	const getfilterdmsg = (m)=>{
+		return m.filter(
+		(msg)=> msg.senderId === selectedConversation._id || msg.receiverId === selectedConversation._id)
+	}
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
+			{console.log("=>>>>> ", messages)}
 			{!loading &&
 				messages.length > 0 &&
-				messages.map((message) => (
+				getfilterdmsg(messages).map((message) => (
 					<div key={message._id} ref={lastMessageRef}>
 						<Message message={message} />
 					</div>
-				))}
+				))
+				}
 
 			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 			{!loading && messages.length === 0 && (
